30f57f7bb0e461d047c0a3a10963d788e9baa25e
MichalBogoryja <m.michal.bz@gmail.com>
2020-06-22 20:45:04 +0200

    Add menu for different movies lists, page for a movie details

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import tmdb_client
+import random
 
 app = Flask(__name__)
 
@@ -10,7 +11,27 @@ def utility_processor():
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
 
+
 @app.route('/')
 def homepage():
-    movies = tmdb_client.get_movies(8)
-    return render_template("homepage.html", movies=movies)
+    available_lists = ['latest', 'now_playing', 'popular', 'top_rated',
+                       'upcoming']
+    selected_list = request.args.get('list_type', "popular")
+    if selected_list not in available_lists:
+        return redirect("/?list_type=popular")
+    movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
+
+    return render_template("homepage.html", movies=movies,
+                           current_list=selected_list,
+                           available_lists=available_lists)
+
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    print(details["backdrop_path"])
+    cast = tmdb_client.get_single_movie_cast(movie_id)[:12]
+    movie_images = tmdb_client.get_movie_images(movie_id)
+    selected_backdrop = random.choice(movie_images['backdrops'])['file_path']
+    return render_template("movie_details.html", movie=details, cast=cast,
+                           selected_backdrop=selected_backdrop)
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,17 +1,28 @@
 import requests
 import random
 
+api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTQ2YjM0N2ZiNzNjNjlmYzQ0ZWYxNT" \
+            "c0YzZjODM4YiIsInN1YiI6IjVlZjA5ZWIzNTY4NDYzMDAzNDAzNTgxMSIsInNjb" \
+            "3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wKUOWsYecNdbSyFBU_Dzd" \
+            "zJEBSLnqIMPtek_eqrUzws"
+
 
 def get_popular_movies():
     endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTQ2YjM0N2ZiNzNjNjlmYzQ0ZW" \
-                "YxNTc0YzZjODM4YiIsInN1YiI6IjVlZjA5ZWIzNTY4NDYzMDAzNDAzNTgxM" \
-                "SIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wKUOWsYec" \
-                "NdbSyFBU_DzdzJEBSLnqIMPtek_eqrUzws"
     headers = {
         "Authorization": f"Bearer {api_token}"
     }
     response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_movies_list(list_name="popular"):
+    endpoint = f"https://api.themoviedb.org/3/movie/{list_name}"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    response.raise_for_status()
     return response.json()
 
 
@@ -20,7 +31,34 @@ def get_poster_url(poster_api_path, size="w342"):
     return f"{base_url}{size}/{poster_api_path}"
 
 
-def get_movies(how_many):
-    data = get_popular_movies()
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_movies(how_many, list_type):
+    data = get_movies_list(list_type)
     random.shuffle(data["results"])
     return data["results"][:how_many]
+
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
